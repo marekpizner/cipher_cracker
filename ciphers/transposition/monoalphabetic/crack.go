448cdd543cecdf7462e68f62efa6a4f2214046f0
marekpizner <m.pizner@gmail.com>
2020-09-03 23:20:57 +0200

    refactor: move majority of code to language_tools for future reuse

--- ciphers/transposition/monoalphabetic/crack.go
+++ ciphers/transposition/monoalphabetic/crack.go
@@ -2,72 +2,10 @@ package monoalphabetic
 
 import (
 	"fmt"
-	"math"
-	"sort"
 	"strings"
 
 	"github.com/khan745/cipher_cracker/language_tools"
 )
-
-func check(s string) map[rune]uint {
-	m := make(map[rune]uint, len(s))
-	for _, r := range s {
-		if r != ' ' {
-			m[r]++
-		}
-	}
-	return m
-}
-
-// TODO: refactor this
-
-func round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
-
-func toFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
-}
-
-func sortAlphabet(aNormal, aSecret, alphabetNormal string) (string, string) {
-	secretAlphabet := ""
-	for _, x := range alphabetNormal {
-		index := language_tools.FindIndexOfString(aNormal, x)
-		secretAlphabet += string(aSecret[index])
-	}
-	return alphabetNormal, secretAlphabet
-}
-
-func getAlphabetsOrderProbability(textSecret, alphabetNormal string, alphabetNormalProbability []language_tools.Alphabet) (string, string) {
-	frequenties := check(textSecret)
-	encryptedAlphabet := []language_tools.Alphabet{}
-	for i, x := range frequenties {
-		encryptedAlphabet = append(encryptedAlphabet, language_tools.Alphabet{Character: string(i), Probability: float32(x)})
-	}
-
-	sort.Slice(alphabetNormalProbability, func(i, j int) bool {
-		return alphabetNormalProbability[i].Probability > alphabetNormalProbability[j].Probability
-	})
-
-	sort.Slice(encryptedAlphabet, func(i, j int) bool {
-		return encryptedAlphabet[i].Probability > encryptedAlphabet[j].Probability
-	})
-
-	alphabetReal := ""
-	for _, y := range alphabetNormalProbability {
-		alphabetReal += strings.ToLower(string(y.Character))
-	}
-
-	alphabetSecret := ""
-	for _, y := range encryptedAlphabet {
-		percentage := float64(y.Probability) / float64(len(textSecret)) * 100
-		percentage = toFixed(percentage, 3)
-		alphabetSecret += string(y.Character)
-	}
-	alphabetReal, alphabetSecret = sortAlphabet(alphabetReal, alphabetSecret, alphabetNormal)
-	return alphabetReal, alphabetSecret
-}
 
 func calculateLocalMaximum(textSecret, alphabetReal, alphabetSecret string, reaQuadgrams map[string]float64, repeatIterations int) (int, string) {
 	maxFitnes := 0
@@ -105,7 +43,7 @@ func Crack(textSecret, alphabetNormal string, realQuadgrams map[string]float64, 
 	textSecret = strings.ToLower(textSecret)
 	alphabetNormal = strings.ToLower(alphabetNormal)
 	alphabetNormal = strings.ReplaceAll(alphabetNormal, " ", "")
-	alphabetNormal, alphabetSecret := getAlphabetsOrderProbability(textSecret, alphabetNormal, alphabetNormalProbability)
+	alphabetNormal, alphabetSecret := language_tools.GetAlphabetsOrderProbability(textSecret, alphabetNormal, alphabetNormalProbability)
 
 	bestScore := 0
 	bestScoreHits := 0
--- language_tools/language_tools.go
+++ language_tools/language_tools.go
@@ -1,10 +1,31 @@
 package language_tools
 
 import (
+	"math"
 	"math/rand"
+	"sort"
 	"strings"
 	"time"
 )
+
+func check(s string) map[rune]uint {
+	m := make(map[rune]uint, len(s))
+	for _, r := range s {
+		if r != ' ' {
+			m[r]++
+		}
+	}
+	return m
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
 
 func Shuffle(src string) string {
 	src = strings.ReplaceAll(src, " ", "")
@@ -33,4 +54,43 @@ func SwapCharactersInAlphabet(alphabet string, char1, char2 rune) string {
 	index2 := FindIndexOfString(alphabet, char2)
 	newAlphabet[index1], newAlphabet[index2] = newAlphabet[index2], newAlphabet[index1]
 	return string(newAlphabet)
+}
+
+func sortAlphabet(aNormal, aSecret, alphabetNormal string) (string, string) {
+	secretAlphabet := ""
+	for _, x := range alphabetNormal {
+		index := FindIndexOfString(aNormal, x)
+		secretAlphabet += string(aSecret[index])
+	}
+	return alphabetNormal, secretAlphabet
+}
+
+func GetAlphabetsOrderProbability(textSecret, alphabetNormal string, alphabetNormalProbability []Alphabet) (string, string) {
+	frequenties := check(textSecret)
+	encryptedAlphabet := []Alphabet{}
+	for i, x := range frequenties {
+		encryptedAlphabet = append(encryptedAlphabet, Alphabet{Character: string(i), Probability: float32(x)})
+	}
+
+	sort.Slice(alphabetNormalProbability, func(i, j int) bool {
+		return alphabetNormalProbability[i].Probability > alphabetNormalProbability[j].Probability
+	})
+
+	sort.Slice(encryptedAlphabet, func(i, j int) bool {
+		return encryptedAlphabet[i].Probability > encryptedAlphabet[j].Probability
+	})
+
+	alphabetReal := ""
+	for _, y := range alphabetNormalProbability {
+		alphabetReal += strings.ToLower(string(y.Character))
+	}
+
+	alphabetSecret := ""
+	for _, y := range encryptedAlphabet {
+		percentage := float64(y.Probability) / float64(len(textSecret)) * 100
+		percentage = toFixed(percentage, 3)
+		alphabetSecret += string(y.Character)
+	}
+	alphabetReal, alphabetSecret = sortAlphabet(alphabetReal, alphabetSecret, alphabetNormal)
+	return alphabetReal, alphabetSecret
 }

0203a67295d1318a7e806367b9bc42e3078851b5
marekpizner <m.pizner@gmail.com>
2020-09-05 18:24:12 +0200

    add to to for cracking viniger cipher

--- ciphers/vigenere/cracker.go
+++ ciphers/vigenere/cracker.go
@@ -3,5 +3,7 @@ package vigenere
 import "github.com/khan745/cipher_cracker/language_tools"
 
 func Crack(textSecret string, realQuadgrams map[string]float64, alphabetNormalProbability []language_tools.Alphabet, alphabets []string) {
-
+	//TODO: crack viniger cipher
+	// 1. find key length
+	// 2. frequency analysis
 }
